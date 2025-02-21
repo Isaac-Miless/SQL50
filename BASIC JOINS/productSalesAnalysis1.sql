76c09ebbca2f09a430728364b8be52b1eb965478
Isaac <ibm4@kent.ac.uk>
2025-02-21 17:48:29 +0000

    Add #7 productSalesAnalysis1

---
+++ BASIC JOINS/productSalesAnalysis1.sql
@@ -0,0 +1,25 @@
+-- Table: Sales
+
+-- +-------------+-------+
+-- | Column Name | Type  |
+-- +-------------+-------+
+-- | sale_id     | int   |
+-- | product_id  | int   |
+-- | year        | int   |
+-- | quantity    | int   |
+-- | price       | int   |
+-- +-------------+-------+
+
+-- Table: Product
+
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | product_id   | int     |
+-- | product_name | varchar |
+-- +--------------+---------+
+
+SELECT product_name, year, price
+FROM Sales s
+    LEFT JOIN Product p
+    ON s.product_id = p.product_id;
